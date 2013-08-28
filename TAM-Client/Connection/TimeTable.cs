7e6b021611631144f6af36a28bfce0ba6e628509
Jenjen1324 <northcodedotno@gmail.com>
2013-08-28 19:06:17 +0200

    Created APP Project

--- TAM-Client/Connection/TimeTable.cs
+++ TAM-Client/Connection/TimeTable.cs
@@ -18,11 +18,14 @@ namespace TAM_Client.Connection
         public static TimeTable Parse(string html)
         {
             Match days = Regex.Match(html, "class\\=\\\"head-day\\\"\\>(\\w+)");
-            Match classes = Regex.Match(html, ".+onMouseOver\\=\\\"tooltip\\(\\'([a-zA-Z0-9\\&\\;\\s]+)\\'\\)\\;\\\"\\s*onMouseOut\\=\\\"kill\\(\\)\\;\\\"\\>\\<strong\\>([a-zA-Z0-9\\&\\#\\;]+)\\<\\/strong\\>([a-zA-Z0-9\\\"\\&\\;]+)(\\<\\/span\\>\\<span\\s*id\\=\\\"[a-z0-9]*\\\"\\s*class\\=\\\"spanright\\s*tt\\-entry\\\"\\>([0-9]+))?");
+            MatchCollection classes = Regex.Matches(html, ".+onMouseOver\\=\\\"tooltip\\(\\'([a-zA-Z0-9\\&\\;\\s]+)\\'\\)\\;\\\"\\s*onMouseOut\\=\\\"kill\\(\\)\\;\\\"\\>\\<strong\\>([a-zA-Z0-9\\&\\#\\;]+)\\<\\/strong\\>([a-zA-Z0-9\\\"\\&\\;]+)(\\<\\/span\\>\\<span\\s*id\\=\\\"[a-z0-9]*\\\"\\s*class\\=\\\"spanright\\s*tt\\-entry\\\"\\>([0-9]+))?");
 
-            return new TimeTable() {
-            name = classes.Groups[0].Captures[1].Value.ToString()
-            };
+            foreach (Match m in classes)
+            {
+                
+            }
+
+            return new TimeTable();
         }
     }
 
